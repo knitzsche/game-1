35578bd8d58ebbe0f3f762816111174bb063dbc0
Kyle Nitzsche <kyle.nitzsche@gcanonical.com>
2016-01-29 16:45:28 -0500

    add libsdl2-gfx-dev lib to pick up graphic primites and mod make accordingly

--- game1/src/main.cpp
+++ game1/src/main.cpp
@@ -4,13 +4,11 @@
 #include <iostream>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL2_gfxPrimitives.h> //install libsdl2-gfx-dev
 #include "res_path.h"
 #include "cleanup.h"
 
-/*
- * Lesson 4: Handling Events
- */
-//Screen attributes
+
 const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
 
@@ -22,51 +20,6 @@ const int SCREEN_HEIGHT = 480;
 void logSDLError(std::ostream &os, const std::string &msg){
 	os << msg << " error: " << SDL_GetError() << std::endl;
 }
-/*
- * Loads an image into a texture on the rendering device
- * @param file The image file to load
- * @param ren The renderer to load the texture onto
- * @return the loaded texture, or nullptr if something went wrong.
- */
-SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren){
-	SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
-	if (texture == nullptr){
-		logSDLError(std::cout, "LoadTexture");
-	}
-	return texture;
-}
-/*
- * Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
- * width and height
- * @param tex The source texture we want to draw
- * @param rend The renderer we want to draw too
- * @param x The x coordinate to draw too
- * @param y The y coordinate to draw too
- * @param w The width of the texture to draw
- * @param h The height of the texture to draw
- */
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h){
-	//Setup the destination rectangle to be at the position we want
-	SDL_Rect dst;
-	dst.x = x;
-	dst.y = y;
-	dst.w = w;
-	dst.h = h;
-	SDL_RenderCopy(ren, tex, NULL, &dst);
-}
-/*
- * Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
- * the texture's width and height
- * @param tex The source texture we want to draw
- * @param rend The renderer we want to draw too
- * @param x The x coordinate to draw too
- * @param y The y coordinate to draw too
- */
-void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y){
-	int w, h;
-	SDL_QueryTexture(tex, NULL, NULL, &w, &h);
-	renderTexture(tex, ren, x, y, w, h);
-}
 
 void addRect(std::shared_ptr<std::vector<std::shared_ptr<SDL_Rect>>> rs) {
 
@@ -75,15 +28,67 @@ void addRect(std::shared_ptr<std::vector<std::shared_ptr<SDL_Rect>>> rs) {
 	r->y = 0;
 	r->w = 50;
 	r->h = 50;
-
 	rs->emplace_back(r);
+} 
 
+struct Circle {
+	int x;
+	int y;
+	int r; // radius
+};
+
+void addCircle(std::shared_ptr<std::vector<std::shared_ptr<Circle>>> cs) {
+
+	std::shared_ptr<Circle> c = std::make_shared<Circle>();
+	c->x = 0;
+	c->y = 0;
+	c->r = 50;
+	cs->emplace_back(c);
 } 
 
 void move(std::shared_ptr<SDL_Rect> r) {
 	int d = 2;
 	r->x = r->x+2;
 	r->y = r->y+2;
+}
+
+void fill_circle(SDL_Surface *surface, int cx, int cy, int radius, Uint32 pixel)
+{
+    std::cout << "radius " << radius << std::endl;
+    // Note that there is more to altering the bitrate of this 
+    // method than just changing this value.  See how pixels are
+    // altered at the following web page for tips:
+    //   http://www.libsdl.org/intro.en/usingvideo.html
+    static const int BPP = 4;
+    std::cout << "== 1" << std::endl;  
+    double r = (double)radius;
+ 
+    for (double dy = 1; dy <= r; dy += 1.0)
+    {
+        // This loop is unrolled a bit, only iterating through half of the
+        // height of the circle.  The result is used to draw a scan line and
+        // its mirror image below it.
+ 
+        // The following formula has been simplified from our original.  We
+        // are using half of the width of the circle because we are provided
+        // with a center and we need left/right coordinates.
+ 
+        double dx = floor(sqrt((2.0 * r * dy) - (dy * dy)));
+        int x = cx - dx;
+ 
+        // Grab a pointer to the left-most pixel for each half of the circle
+        Uint8 *target_pixel_a = (Uint8 *)surface->pixels + ((int)(cy + r - dy)) * surface->pitch + x * BPP;
+        Uint8 *target_pixel_b = (Uint8 *)surface->pixels + ((int)(cy - r + dy)) * surface->pitch + x * BPP;
+ 
+        for (; x <= cx + dx; x++)
+        {
+            *(Uint32 *)target_pixel_a = pixel;
+            *(Uint32 *)target_pixel_b = pixel;
+            std::cout << "== 2" << std::endl;  
+            target_pixel_a += BPP;
+            target_pixel_b += BPP;
+        }
+    }
 }
 
 int main(int, char**){
@@ -110,54 +115,48 @@ int main(int, char**){
 		SDL_Quit();
 		return 1;
 	}
-	//The texture we'll be using
-	SDL_Texture *image = loadTexture("fr.bmp", renderer);
-	if (image == nullptr){
-		cleanup(image, renderer, window);
-		IMG_Quit();
+	SDL_Surface *surface = SDL_GetWindowSurface(window);
+	if (surface == nullptr){
+		logSDLError(std::cout, "CreateSurface");
+		cleanup(window, renderer);
 		SDL_Quit();
 		return 1;
 	}
 
-	//Our texture size won't change, so we can get it here
-	//instead of constantly allocating/deleting ints in the loop
-	int iW, iH;
-	SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
-	int x = SCREEN_WIDTH / 2 - iW / 2;
-	int y = SCREEN_HEIGHT / 2 - iH / 2;
-
+	// a rect
 	SDL_Rect rect;
 	rect.x = 0;
 	rect.y = 0;
 	rect.w = 32;
 	rect.h = 32;
 
+	// vector of dynamically created resctangles
 	std::shared_ptr<std::vector<std::shared_ptr<SDL_Rect>>> rects = std::make_shared<std::vector<std::shared_ptr<SDL_Rect>>>();
 
-	//Our event structure
+	// vector of dynamically created resctangles
+	std::shared_ptr<std::vector<std::shared_ptr<Circle>>> circles = std::make_shared<std::vector<std::shared_ptr<Circle>>>();
+
 	SDL_Event e;
-	//For tracking if we want to quit
 	bool quit = false;
 	while (!quit){
-		//Read any events that occured, for now we'll just quit if any event occurs
 		while (SDL_PollEvent(&e)){
-			//If user closes the window
+			// user closes the window
 			if (e.type == SDL_QUIT){
 				quit = true;
 			}
-			//If user presses any key
+			// user presses any key
 			if (e.type == SDL_KEYDOWN){
 				quit = true;
 			}
-			//If user clicks the mouse
+			// user clicks the mouse
 			if (e.type == SDL_MOUSEBUTTONDOWN){
-				std::cout << "add" << std::endl;
+				//std::cout << "add" << std::endl;
 				addRect(rects);
-				std::cout << "add size"<< rects->size()  << std::endl;
-				//quit = true;
+				addCircle(circles);
+				//std::cout << "add size"<< rects->size()  << std::endl;
 			}
 		}
-		//Rendering
+
 		SDL_SetRenderDrawColor( renderer, 20, 20, 20, 255 );
 		SDL_RenderClear(renderer);
 
@@ -166,19 +165,21 @@ int main(int, char**){
 			move(r);
 			SDL_RenderFillRect( renderer, r.get() );
 		}
+		SDL_SetRenderDrawColor( renderer, 200, 200, 0, 255 );
+		for( std::shared_ptr<Circle> &c : *circles ) {
+			//move(r);
+			int result = filledCircleColor(renderer, c->x, c->y, c->r, 0xFF0000FF);
+
+		}
 		rect.x++;
 		rect.y++;
 		SDL_RenderFillRect( renderer, &rect );
-		//Draw the image
-		//renderTexture(image, renderer, x, y);
-		//x++;
-		//y++;
 
 		//Update the screen
 		SDL_RenderPresent(renderer);
 	}
-	//Destroy the various items
-	cleanup(image, renderer, window);
+
+	cleanup( renderer, window);
 	IMG_Quit();
 	SDL_Quit();
 
